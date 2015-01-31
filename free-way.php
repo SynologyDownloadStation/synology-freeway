f8cd1d5422237c8ebddef87ae10d45e53735d4fa
scriptshub <noreplys@secure-mail.cc>
2015-01-31 19:34:43 +0100

    added a method that tries to determine the filename of the file. oboom should be working now.

--- free-way.php
+++ free-way.php
@@ -8,11 +8,14 @@ define('ERR_REQUIRED_PREMIUM', 115);
 define('ERR_NOT_SUPPORT_TYPE', 116);
 define('DOWNLOAD_ISQUERYAGAIN', 'isqueryagain');
 define('DOWNLOAD_STATION_USER_AGENT', "Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)");
-define('DOWNLOAD_URL', 'downloadurl'); 			// will contain the real download url
+define('DOWNLOAD_URL', 'downloadurl'); 				// will contain the real download url
+define('DOWNLOAD_FILENAME', 'filename'); 			// Saved file name
+define('DOWNLOAD_ISPARALLELDOWNLOAD', 'isparalleldownload');	//Task can download parallel flag.
 
 class FreeWayFileHost {
 	private $url, $user, $pass, $hostInfo, $cookie = "/tmp/freeWay.cookie";
-	
+	private $type = "premium";	
+
 	public function __construct($url, $user, $pass, $hostInfo) {
 		$this->url = $url;
 		$this->user = $user;
@@ -22,15 +25,77 @@ class FreeWayFileHost {
 	
 	public function GetDownloadInfo() {
 		if(!file_exists($this->cookie))
+		{
 			$this->Verify(false);
+		}
 
 		$res = $this->Get("https://www.free-way.me/load.php?url=".urlencode($this->url)."&user=".$this->user."&pw=".$this->pass."&multiget=4", false, true);
-		
+		$downloadUrl = (string)$res;
+		$name = $this->TryToGetFilename($downloadUrl);
+
 		return array(
-		    DOWNLOAD_URL => $res,
-		);
-	}
+			    DOWNLOAD_URL => $downloadUrl,
+			    DOWNLOAD_FILENAME => empty($name) ? NULL : $name,
+			    DOWNLOAD_ISPARALLELDOWNLOAD => ($this->type == "premium") ? true : false,
+			);
+	}	
+
+	public function TryToGetFilename($downloadUrl) 
+	{
+		$ch = curl_init($downloadUrl);
+		curl_setopt ($ch, CURLOPT_HEADER, TRUE); 
+		curl_setopt ($ch, CURLOPT_NOBODY, TRUE);
+		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt ($ch, CURLOPT_BINARYTRANSFER, 0);	
+
+		// limit range to save time (doesn't seem possible on netload)
+		if (!strstr($downloadUrl, "netload.in/"))
+		{
+			curl_setopt ($ch, CURLOPT_RANGE, "0-100");
+		}
+
+		// do request and save response
+		$response = curl_exec($ch);
+		curl_close($ch);
+
+		// build header array
+		$header = array();
+		$header_text = substr($response, 0, strpos($response, "\r\n\r\n"));
+		foreach (explode("\r\n", $header_text) as $i => $line)
+		{
+			if ($i === 0)
+			{
+			    $header['http_code'] = $line;
+			}
+			else
+			{
+			    list ($key, $value) = explode(': ', $line);
+
+			    $header[$key] = $value;
+			}
+		}
 	
+		// try to read-out filename from HTTP header
+		if (!empty($header["Content-Disposition"]))
+		{
+			$nameParts = explode('=', $header["Content-Disposition"]);
+			if ($nameParts[1]) 
+			{
+				$fileName = trim($nameParts[1],'";\' ');
+		
+				// remove utf-8 prefix if it exists
+				if (substr($fileName, 0, 7 ) === "UTF-8''") 
+				{
+					$fileName = substr($fileName, 7);
+				}
+			
+				return $fileName;
+			}
+		}
+
+		return;
+	}
+
 	public function Verify($ClearCookie) {
 		$loginURL = "https://www.free-way.me/ajax/jd.php?id=1&user=".urlencode($this->user)."&pass=".urlencode($this->pass);
 		$res = $this->Get($loginURL,true);
@@ -50,9 +115,11 @@ class FreeWayFileHost {
 			$testRes = (array) json_decode($this->Get($testURL,true));
 			if ($testRes["premium"] == "Free")
 			{
+				$this->type = "free";
 				return USER_IS_FREE;
 			}
 			else {
+				$this->type = "premium";
 				return USER_IS_PREMIUM;
 			}
 		}
